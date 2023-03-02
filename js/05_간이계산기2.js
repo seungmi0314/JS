9ae4b47476f7d1a4fabe8c5fcd0eec785112586e
BAEK <bbsm0314@gmail.com>
2023-03-02 14:31:47 +0900

    간이계산기

---
+++ js/05_간이계산기2.js
@@ -0,0 +1,23 @@
+// 전역 변수
+const num1 = document.getElementById("num1"); // 요소 추적 O, 값, 속성 추적 X
+const num2 = document.getElementById("num2");
+const result = document.getElementById("result");
+
+
+function calc(op){
+    // op 에는 +, -, *, /, %  중 하나가 전달 됨
+
+    const n1 = Number(num1.value);  //형변환?
+    const n2 = Number(num2.value);  //형변환?
+    
+
+    switch(op) {
+        case '+' : result.innerText = n1 + n2; break;
+        case '-' : result.innerText = n1 - n2; break;
+        case '*' : result.innerText = n1 * n2; break;
+        case '/' : result.innerText = n1 / n2; break;
+        case '%' : result.innerText = n1 % n2; break;
+
+
+    }
+}
